132fedc2d6d3c5aee05ced809772cc578fb51ec1
divinejoshua <divineuche399@gmail.com>
2023-12-27 20:55:25 +0000

    Added Models

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RunGroopWebApp.Models
+{
+	public class AppUser
+	{
+		public int? Pace { get; set; }
+		public int? Millage { get; set; }
+		public Address? Address { get; set; }
+		public ICollection<Club>? Clubs { get; set; }
+		public ICollection<Race>? Races {get; set;}
+	}
+}
+
